f2281efe700688780b100ccc77a3434e0e6afe26
Ian Fong <ian.fong8@gmail.com>
2020-11-24 13:02:46 -0800

    renamed some model fields and updated menu.html

--- bpsite/orders/models.py
+++ bpsite/orders/models.py
@@ -11,8 +11,8 @@ class Ingredients(models.Model):#maybe a silly name but temp for lack of better 
 	quantity = models.IntegerField(default=0)
 
 class Order(models.Model):
-	order_items = models.IntegerField(default=0) # just a placeholder field to not deal with list/foreign keys
-	order_placed = models.DateTimeField('date created')
+	items = models.IntegerField(default=0) # just a placeholder field to not deal with list/foreign keys
+	time_placed = models.DateTimeField('date created')
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	completion_eta = models.IntegerField(default=0)
 
@@ -21,8 +21,8 @@ class Order(models.Model):
 
 class Item(models.Model):
 	item_name = models.CharField(max_length=50)
-	item_quantity = models.IntegerField(default=0)
-	item_prep = models.IntegerField(default=0)
+	quantity = models.IntegerField(default=0)
+	prep_time = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.item_name
