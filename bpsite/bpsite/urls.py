1805b2e1385ddb7127f01cd5908db10b89f46ca1
Ian Fong <ian.fong8@gmail.com>
2020-11-23 20:49:03 -0800

    added some super basic placeholder html

--- bpsite/bpsite/urls.py
+++ bpsite/bpsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from orders import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('orders/', include('orders.urls'))
+    path('orders/', include('orders.urls')),
+    path('home/', views.home, name="home"),
+    path('about/', views.about, name="about"),
 ]
--- bpsite/orders/views.py
+++ bpsite/orders/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def index(request):
-	return HttpResponse("test view for orders index")
-# Create your views here.
+	return HttpResponse("index")
+
+def home(request):
+	return render(request, 'bpsite/base.html', {})
+
+def about(request):
+	return render(request, 'bpsite/home.html', {})
+
+class TestView(TemplateView):
+	template_name = "about.html"
